066098ea07eb42922df172fa99a1d9db737f18f5
hanna-willis <107685700+hanna-willis@users.noreply.github.com>
2022-10-09 11:32:24 -0500

    Update combine_scans_and_ROI.sh

--- combine_scans_and_ROI.sh
+++ combine_scans_and_ROI.sh
@@ -8,23 +8,25 @@
 set -x 
 set -e
 
+# Set up directory 
+rois_loc=`jq -r '.rois_loc' config.json`
+
 # Parse input file names from config.json. 
 rois=`jq -r '.rois' config.json`
 dwi=`jq -r '.dwi' config.json`
 t1=`jq -r '.t1' config.json`
 
 # Make an output directory.
-mkdir ./output
-mkdir ./output/rois
+mkdir output output/rois
 
 # Copy files from the roi1 location to the new location and rename.
 for roiname in $(ls $rois); do	
 	# Copy the rois from the first ROI to the output ROI directory.
-	cp -r $rois/$roiname output/rois/
+	cp -r ${rois_loc}/$rois/$roiname ./output/rois/
 done
 
 # Copy diff and t1 into new location
-cp -r ${dwi} output/rois
-cp -r ${t1} output/rois
+cp -r ${dwi} ./output/rois
+cp -r ${t1} ./output/rois
 
 
